8a876d186df9fd0c303e9e56b419fde72876e81d
halspring <hal.webworks@gmail.com>
2017-01-24 19:06:00 +0900

    dbに接続できない問題を解決

--- config/database.php
+++ config/database.php
@@ -13,6 +13,7 @@ return [
             "charset"   => "utf8",
             "collation" => "utf8_unicode_ci",
             "prefix"    => "",
+            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
             "strict"    => false,],],
     "migrations" => "migrations",
     "redis" => [
